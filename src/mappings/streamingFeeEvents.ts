61c57ca1cd84c709c683b7bd1c19a17e84684651
tyler-cranmer <tyler.cranmer@outlook.com>
2022-01-10 15:43:53 -0700

    commit before changes

--- src/mappings/streamingFeeEvents.ts
+++ src/mappings/streamingFeeEvents.ts
@@ -5,26 +5,6 @@ import { fetchManager } from '../utils/setToken';
 import { createFee, createManager } from '../utils/create';
 import { createGenericId } from '../utils';
 
-// export function handleFeeActualized(event: FeeActualized): void {
-//   let entity = new Fee(
-//     event.transaction.hash.toHex() + '-' + event.logIndex.toString()
-//   );
-//   let setTokenAddress = event.params._setToken
-
-//   let feeEntity = createFee(event.transaction.hash.toHex() + '-' + event.logIndex.toString(), event.block.timestamp, event.params._managerFee, event.params._protocolFee)
-
-//   let currentManager = Manager.load(fetchManager(setTokenAddress))
-
-//   if (currentManager == null) {
-//     currentManager = createManager(fetchManager(setTokenAddress), setTokenAddress)
-//   }
-
-//   currentManager.save()
-//   feeEntity.manager = currentManager.id;
-
-//   feeEntity.save();
-// }
-
 export function handleFeeActualized(event: FeeActualized): void {
   const id: string = createGenericId(event);
   const timestamp: BigInt = event.block.timestamp;
